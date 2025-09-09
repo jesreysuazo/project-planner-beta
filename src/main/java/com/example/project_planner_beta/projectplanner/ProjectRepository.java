537a01e583da051fff8e88619042fcc522be8629
jesreysuazo <85920977+jesreysuazo@users.noreply.github.com>
2025-09-09 15:56:42 +0800

    Delete src directory

--- src/main/java/com/example/project_planner_beta/projectplanner/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.project_planner_beta.projectplanner;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-    Project findByCode(String code);
-}
