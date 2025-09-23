28c68727e524789329cbbc26d6ad37db421c5fc0
jesreysuazo <85920977+jesreysuazo@users.noreply.github.com>
2025-09-23 10:22:59 +0800

    Delete src directory

--- src/main/java/com/example/project_planner_beta/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.project_planner_beta.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/example/project_planner_beta/projectplanner/Task/TaskStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.project_planner_beta.projectplanner.Task;
-
-public enum TaskStatus {
-    NOT_STARTED,
-    IN_PROGRESS,
-    DONE
-}
